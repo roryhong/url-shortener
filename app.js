e6c949b8e58e5d341daba964d489f2dd476dd360
roryhong <hongst1995@gmail.com>
2021-07-25 14:30:36 +0800

    add font page ui

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+require('./config/mongoose')
+
+app.engine('hbs', exphbs({defaultLayout : 'main', extname : '.hbs'}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
 app.get('/', (req , res) => {
-    res.send('hello')
+    res.render('index')
 })
 
 app.listen(port , () => {
