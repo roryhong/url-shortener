fba4617c50dc6cc2e2bcb3449eab764d0d54ae46
roryhong <hongst1995@gmail.com>
2021-08-03 18:14:36 +0800

    modify code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const router = require('./routes')
 const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
